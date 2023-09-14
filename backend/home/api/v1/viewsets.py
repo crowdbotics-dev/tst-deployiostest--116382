86998eb7b620def0f6cbc039ffa17ee5c698f544
crowdbotics-dev <engineering@crowdbotics.com>
2023-09-14 11:12:40 +0000

    Update models

--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,7 @@
+from rest_framework import viewsets
+from home.models import Javed,Javed,Javed
+from .serializers import JavedSerializer,JavedSerializer,JavedSerializer
+from rest_framework import authentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
@@ -28,3 +32,8 @@ class LoginViewSet(ViewSet):
         token, created = Token.objects.get_or_create(user=user)
         user_serializer = UserSerializer(user)
         return Response({"token": token.key, "user": user_serializer.data})
+
+class JavedViewSet(viewsets.ModelViewSet):
+    serializer_class = JavedSerializer
+    authentication_classes = (authentication.SessionAuthentication, authentication.TokenAuthentication)
+    queryset = Javed.objects.all()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Javed(models.Model):
+    'Generated Model'
+    wert = models.BigIntegerField(null=True,blank=True,)
